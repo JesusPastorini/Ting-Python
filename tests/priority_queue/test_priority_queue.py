ae9501bf1f67ebb294eea551ff268ef441181f87
JuniorPastorini <juniorpastorini_@hotmail.com>
2023-11-07 03:35:00 -0300

    req 6 em curso, 7 ok

--- tests/priority_queue/test_priority_queue.py
+++ tests/priority_queue/test_priority_queue.py
@@ -1,5 +1,9 @@
-# from ting_file_management.priority_queue import PriorityQueue
+from ting_file_management.priority_queue import PriorityQueue
 
 
 def test_basic_priority_queueing():
-    """Aqui irá sua implementação"""
+    pq = PriorityQueue()
+    pq.enqueue("Arquivo 1", ["lin 1", "lin 2", "lin 3", "lin 4"])
+    pq.enqueue("Arquivo 2", ["lin 1", "lin 2", "lin 3", "lin 4", "lin 5"])
+    assert pq.search(0) == "Arquivo 1"
+    assert pq.search(1) == "Arquivo 2"
--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,5 +1,23 @@
 def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+    result = []
+
+    for item in instance.items:
+        filename = item["nome_do_arquivo"]
+        lines = item["linhas_do_arquivo"]
+
+        occurrences = []
+        for line_num, line in enumerate(lines, start=1):
+            if word.lower() in line.lower():  # busca case insensitive
+                occurrences.append({"linha": line_num})
+
+        if occurrences:
+            result.append({
+                "palavra": word,
+                "arquivo": filename,
+                "ocorrencias": occurrences
+            })
+
+    return result
 
 
 def search_by_word(word, instance):
